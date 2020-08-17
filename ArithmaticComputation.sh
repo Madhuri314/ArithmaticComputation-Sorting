074e08388eba8888ded5e8f76f9db8ddbec5e2f1
Madhuri Jagdeo <madhuri.jagdeo21@gmail.com>
2020-08-17 16:13:02 +0530

    Computed values in ascending order

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -24,4 +24,17 @@ do
         arr[counter++]=${arithmaticDictionary[$i]}
 done
 
+for (( j=0;j<4;j++ ))
+do
+        for (( k=0;k<4-j-1;k++ ))
+        do
+                if [ ${arr[k]} -gt ${arr[$(( k+1 ))]} ]
+                then
+                        temp=${arr[k]}
+                        arr[$k]=${arr[$(( k+1 ))]}
+                        arr[$(( k+1 ))]=$temp
+                fi
+        done
+done
+echo "Sorted :"
 echo ${arr[@]}
